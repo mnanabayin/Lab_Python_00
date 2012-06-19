90029ecb7e85d7236d7daf1ec730feed55beefb6
AITI Student <aiti-student@AITI-74.(none)>
2012-06-19 15:47:16 +0000

    committing my python solutions

---
+++ Day1.py
@@ -0,0 +1,12 @@
+import math
+print 'Nanabayin Menyah'
+print 'Hello World'
+a = 95
+print a
+
+print '11/2 is ', 11/2
+print '11.0/2 is', 11.0/2
+print '3^27 is', 3**27
+print '(134533 + 6889980 - 84849)^3 is ',(134533 + 6889980 - 84849)**3 
+print 'The average time per meter is ',45/(7.9*1000),' minutes/metre'
+print 'The shortest distance is ',math.sqrt(((4.7*0.621371)**2) + ((9.4*0.621371)**2)),' miles'
